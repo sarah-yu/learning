a99e6e221fc8bab33777e2edb03b4e5cfae248a9
Sarah Yu <sarahyu0692@gmail.com>
2017-12-22 12:02:30 -0500

    create ToDoList component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <to-do-list></to-do-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import the components you want to use
+import ToDoList from './components/ToDoList';
 
 export default {
   name: 'app',
+  // reference the components you want to use in the components property
   components: {
-    HelloWorld,
+    ToDoList,
   },
+
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,19 @@
+<!--  ToDoList Component -->
+<!--  each component consists a template, component class, and style section -->
+
+<template>
+  <div>
+    <ul>
+        <li> Todo A </li>
+        <li> Todo B </li>
+        <li> Todo C </li>
+    </ul>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+export default {};
+</script>
+
+<style>
+</style>
